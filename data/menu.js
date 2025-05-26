3f0e8129c5c37c0803db94c52d8e8da834cddfe2
PavelintheMatrix <166340133+chamalepavel@users.noreply.github.com>
2025-05-25 21:30:20 -0700

    menu.js

---
+++ data/menu.js
@@ -0,0 +1,20 @@
+const menu = {
+  entradas: [
+    { id: 1, nombre: 'Guacamole con totopos', precio: 5.00 },
+    { id: 2, nombre: 'Chicharrones de la casa', precio: 6.50 }
+  ],
+  'platos-fuertes': [
+    { id: 3, nombre: 'Cochinita pibil', precio: 12.00 },
+    { id: 4, nombre: 'Tacos al pastor', precio: 10.00 }
+  ],
+  postres: [
+    { id: 5, nombre: 'Flan de cajeta', precio: 4.00 },
+    { id: 6, nombre: 'Pastel tres leches', precio: 4.50 }
+  ],
+  bebidas: [
+    { id: 7, nombre: 'Agua de horchata', precio: 2.00 },
+    { id: 8, nombre: 'Refresco', precio: 1.50 }
+  ]
+};
+
+module.exports = menu;
